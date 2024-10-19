086cda2ef4fa57307379b54dacfc09b6b1aa1a89
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-19 13:22:42 +0300

    billfrom

--- components/bill-form/index.jsx
+++ components/bill-form/index.jsx
@@ -1,3 +1,23 @@
+import css from './bill-form.css'
+
 export default function BillForm() {
-  return <h1>sdsa</h1>;
+  return (
+      <div className="billFrom">
+        <h3>Fatura Gönderen</h3>
+        <label htmlFor="fromStreet">Sokak</label>
+        <input type="text" id='fromStreet' name="fromStreet" />
+        <div className="city">
+        <div>
+          <label htmlFor="fromCity">Şehir</label>
+          <input type="text" id='fromCity' name="fromCity" />
+        </div>
+        <div>
+          <label htmlFor="fromPostCode">Posta Kodu</label>
+          <input type="text" id='fromPostCode' name='fromPostCode' />
+          </div>
+        </div>
+        <label htmlFor="fromCountry">Ülke</label>
+        <input type="text" id='fromCountry' name="fromCountry"/>
+      </div>
+  )
 }
