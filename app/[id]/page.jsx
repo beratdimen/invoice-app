db3916e5ef6ff155e762f456144e44e71008d2fb
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-20 12:25:36 +0300

    details css fix

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,10 +1,14 @@
 import Details from "@/components/details";
+import Header from "@/components/header/page";
 
 export default function DetailsPage(){
 
   return (
     <>
-      <Details />
+      <div style={{display: "flex"}} className="detailsPage">
+        <Header />
+        <Details />
+      </div>
     </>
   )
 }
