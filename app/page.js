447fa18d399a16cd279563ce6c5432482f0615e2
Berat Dimen <beratefedimen@hotmail.com>
2024-10-20 20:17:32 +0300

    deneme commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
 import InvoicePAge from "./invoice/page";
 
-export default async function Home() {
-  const response = await fetch(
-    " https://invoiceapp.bariscakdi.com.tr/api/User/GetUsers"
-  );
-  const data = await response.json();
-  console.log(data);
+export default function Home() {
   return (
     <>
       <InvoicePAge />
