e11bde4ae6b342708a9fb86c9f8e3ee805dde9f3
Berat Dimen <beratefedimen@hotmail.com>
2024-10-22 13:10:56 +0300

    güncelleme yaptım

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/header";
 import { getInvoices } from "@/utils/service";
 import InvoicesList from "@/components/invoices-list";
 import MainPage from "@/components/main-page";
-import "./home.css"
+import "./home.css";
 
 export default async function Home() {
   const data = await getInvoices();
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <div className="container">
       <Header />
-      
+
       <div className="main-page-content">
         <MainPage />
         <InvoicesList data={data} />
