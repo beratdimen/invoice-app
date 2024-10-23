beff10b5fc4bb08bc069d9ce3e3589c29563fb45
Gokhan <gokhanunlu94@gmail.com>
2024-10-23 03:36:20 +0300

    details-header css fix

--- components/details/index.jsx
+++ components/details/index.jsx
@@ -13,124 +13,121 @@ export default async function Details({ data }) {
         </Link>
       </div>
 
-      <div className="desktopButtons">
-        <div className="status">
-          <p>Status</p>
-
-          <div className="situation">
-            <p
-              className={
-                data.paymentStatus == 2
-                  ? "pending"
+      <div className="contentContainer">
+        <div className="desktopButtons">
+          <div className="status">
+            <p>Status</p>
+            <div className="situation">
+              <p
+                className={
+                  data.paymentStatus == 2
+                    ? "pending"
+                    : data.paymentStatus == 3
+                      ? "paid"
+                      : "draft"
+                }
+              >
+                {data.paymentStatus == 2
+                  ? " ● Pending"
                   : data.paymentStatus == 3
-                  ? "paid"
-                  : "draft"
-              }
-            >
-              {data.paymentStatus == 2
-                ? " ● Pending"
-                : data.paymentStatus == 3
-                ? "● Paid"
-                : "● Draft"}
-            </p>
+                    ? "● Paid"
+                    : "● Draft"}
+              </p>
+            </div>
+          </div>
+          <div className="detailsButtons desktop">
+            <button>Düzenle</button>
+            <DetailDeleteModal data={data} />
+            <button>Ödeme Yapıldı</button>
           </div>
         </div>
-        <div className="detailsButtons desktop">
+        <div className="idAddress">
+          <div className="billIdnCat">
+                <h3>
+                  <strong>{data?.invoiceName}</strong>
+                </h3>
+                <p>{data?.description}placeholder</p>
+          </div>
+          <div className="companyAddress">
+            <p>placeholder</p>
+            <p>placeholder</p>
+            <p>placeholder</p>
+            <p>placeholder</p>
+          </div>
+        </div>
+        <div className="desktopAddressMail">
+          <div className="desktopAdress">
+        
+            <div className="dateDue">
+              <div className="billDate">
+                <p>Fatura Tarihi</p>
+                <strong>placeholder</strong>
+              </div>
+              <div className="billDate">
+                <p>Ödeme Tarihi</p>
+                <strong>placeholder</strong>
+              </div>
+            </div>
+            <div className="customerInfo">
+              <div className="customer">
+                <p>Fatura Sahibi</p>
+                <strong>placeholder</strong>
+              </div>
+              <div className="companyAddress">
+                <p>{data?.client?.address}</p>
+                <p>{data?.client?.city}</p>
+                <p>{data?.client?.postCode}</p>
+                <p>{data?.client?.country}</p>
+              </div>
+            </div>
+          </div>
+          <div className="customerEmail">
+            <p>Gönderilen</p>
+            <strong>{data?.client?.email}</strong>
+          </div>
+        </div>
+        <div className="cart">
+          <div className="cartSections">
+            <p>Ürün Adı</p>
+            <p>Adet</p>
+            <p>Fiyat</p>
+            <p>Toplam</p>
+          </div>
+          {data?.items.map((item, index) => (
+            <div className="cartItemDesktop" key={index}>
+              <strong>{item?.name}</strong>
+              <p>{item?.quantity}</p>
+              <p>£{item?.price}</p>
+              <strong>£{item?.quantity * item?.price}</strong>
+            </div>
+          ))}
+          {data?.items.map((item, index) => (
+            <div className="cartItem" key={index}>
+              <div className="item">
+                <strong>{item?.name}</strong>
+                <p>
+                  {item?.quantity} x £ {item?.price}
+                </p>
+              </div>
+              <strong>£ {item?.quantity * item?.price}</strong>
+            </div>
+          ))}
+          <div className="grandTotal">
+            <p>Ödenecek Tutar</p>
+            <h2>
+              £
+              {data?.items?.reduce((acc, item) => {
+                return acc + item.price * item.quantity;
+              }, 0)}
+            </h2>
+          </div>
+        </div>
+      </div>
+        <div className="detailsButtons mobile">
           <button>Düzenle</button>
           <DetailDeleteModal data={data} />
           <button>Ödeme Yapıldı</button>
         </div>
-      </div>
-
-      <div className="desktopAdress">
-        <div className="billIdnCat">
-          <h3>
-            <strong>{data?.invoiceName}</strong>
-          </h3>
-          <p>{data?.description}</p>
-        </div>
-        <div className="companyAddress">
-          <p>{data?.client?.address}</p>
-          <p>{data?.client?.city}</p>
-          <p>{data?.client?.postCode}</p>
-          <p>{data?.client?.country}</p>
-        </div>
-      </div>
-
-      <div className="desktopAddressMail">
-        {/* <Customer /> */}
-
-        <div className="customerEmail">
-          <p>Gönderilen</p>
-          <strong>{data?.client?.email}</strong>
-        </div>
-      </div>
-
-      <div className="cart">
-        <div className="cartSections">
-          <p>Ürün Adı</p>
-          <p>Adet</p>
-          <p>Fiyat</p>
-          <p>Toplam</p>
-        </div>
-
-        {data?.items.map((item, index) => (
-          <div className="cartItemDesktop" key={index}>
-            <input
-              type="text"
-              name="itemName"
-              readOnly
-              defaultValue={item?.name}
-            />
-            <input
-              type="text"
-              name="quantity"
-              readOnly
-              defaultValue={item?.quantity}
-            />
-            <input
-              type="text"
-              name="price"
-              readOnly
-              defaultValue={`£${item?.price}`}
-            />
-            <input
-              type="text"
-              name="total"
-              readOnly
-              defaultValue={`£${item?.quantity * item?.price}`}
-            />
-          </div>
-        ))}
-
-        {data?.items.map((item, index) => (
-          <div className="cartItem" key={index}>
-            <div className="item">
-              <strong>{item?.name}</strong>
-              <p>
-                {item?.quantity} x £ {item?.price}
-              </p>
-            </div>
-            <strong>£ {item?.quantity * item?.price}</strong>
-          </div>
-        ))}
-
-        <div className="grandTotal">
-          <p>Ödenecek Tutar</p>
-          <h2>
-            £
-            {data?.items?.reduce((acc, item) => {
-              return acc + item.price * item.quantity;
-            }, 0)}
-          </h2>
-        </div>
-      </div>
-
-      <div className="detailsButtons mobile">
-        <button>Düzenle</button>
-        <DetailDeleteModal data={data} />
-        <button>Ödeme Yapıldı</button>
-      </div>
     </div>
   );
 }
